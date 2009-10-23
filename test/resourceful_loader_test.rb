f45ef26ebb7c9b3d9a36b4d46337e2a13e86953c
Jacob Rothstein <github@jacobrothstein.com>
2009-10-23 00:12:21 -0700

    clean up tests, use TestRig gem

--- test/resourceful_loader_test.rb
+++ test/resourceful_loader_test.rb
@@ -1,84 +1,85 @@
-require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
-require 'flexmock'
+require File.instance_eval { expand_path join(dirname(__FILE__), 'test_helper') }
+require 'action_controller'
 require 'resourceful_loader'
 
-class ResourcefulController < ActionController::Base
+class Object
+  def eigenclass
+    class << self; self; end
+  end
+end
+
+class BlankController < ActionController::Base
   include ResourcefulLoader
-  attr_accessor :foo
 end
 
 class Foo; end
 
 class ResourcefulLoaderTest < Test::Unit::TestCase
   include FlexMock::TestCase
-  def setup
-    super
-    @controller = ResourcefulController.new
-    @request = ActionController::TestRequest.new
-    @response = ActionController::TestResponse.new
-  end
 
-  context "with a blank ActionController::Base" do
+  context "A blank AC:B" do
     should 'have a #load_resource class method' do
-      assert @controller.class.methods.include?('load_resource')
-      assert @controller.class.respond_to?(:load_resource)
+      assert BlankController.respond_to?(:load_resource)
+    end
+  end
+  
+  context 'With a controller that has a basic load_resource macro' do
+    before do
+      @controller = BlankController.new
+      @controller.eigenclass.load_resource :foo
     end
 
-    context 'with a preloaded resource foo' do
-      setup do
-        @controller.class.load_resource :foo
+    should 'append #load_foo on the before filters' do
+      assert @controller.eigenclass.before_filters.include?(:load_foo)
+    end
+    
+    should 'define #load_foo' do
+      assert @controller.private_methods.include?("load_foo")
+    end
+    
+    context 'on call to load_foo' do
+      should 'call Foo.find_by_id(params[:foo])' do
+        flexmock(@controller, :params => {'foo_id' => 'bar'})
+        flexmock(Foo).should_receive(:find_by_id).once.with('bar').and_return('wibble')
+        @controller.send :load_foo
+        assert_equal 'wibble', @controller.instance_variable_get(:@foo)
       end
-
-      should 'have a private method #load_foo' do
-        assert @controller.private_methods.include?('load_foo')
-      end
-
-      should 'append #load_foo on the before filters' do
-        assert @controller.class.before_filters.include?(:load_foo)
-      end
-
-      context 'load_foo' do
-        should 'call Foo.find_by_id(params[:foo])' do
-          flexmock(@controller, :params => {'foo_id' => 'bar'})
-          flexmock(Foo).should_receive(:find_by_id).once.with('bar').and_return('wibble')
-          @controller.send :load_foo
-          assert_equal 'wibble', @controller.foo
-        end
+    end
+  end
+  
+  context 'With a controller that specifies some options' do
+    before do
+      @controller = BlankController.new
+      @filter_options = {"only" => [:some, :method, :names]}
+      @controller.eigenclass.load_resource :foo, @filter_options.merge(:by => :id, :method => :find_by_searching)
+    end
+    
+    should 'pass the filter options except :by and :method through to before_filter' do
+      filter = @controller.eigenclass.filter_chain.detect {|filter| filter.method == :load_foo}
+      assert_options @filter_options, filter
+    end
+    
+    should 'call the method with params[options[:by]]' do
+      flexmock(@controller, :params => {:id => 'bar'})
+      flexmock(Foo).should_receive(:find_by_searching).once.with('bar').and_return('wibble')
+      @controller.send :load_foo
+      assert_equal 'wibble', @controller.instance_variable_get(:@foo)
+    end
+  end
+  
+  context 'with a block' do
+    before do
+      @controller = BlankController.new
+      @controller.eigenclass.load_resource :foo do |foo_param|
+        "test response #{foo_param}"
       end
     end
     
-    context 'with an options hash' do
-      setup do
-        @options = {'only' => [:some, :method, :names]}
-        @controller.class.load_resource :foo, @options.merge(:by => :id, :method => :find_by_searching)
-      end
-      
-      should 'pass the options except :by and :method through to before_filter' do
-        filter = @controller.class.filter_chain.detect {|filter| filter.method == :load_foo}
-        assert_equal @options, filter.options
-      end
-      
-      should 'eval the method with params[options[:by]]' do
-        flexmock(@controller, :params => {:id => 'bar'})
-        flexmock(Foo).should_receive(:find_by_searching).once.with('bar').and_return('wibble')
+    context 'load_foo' do
+      should 'eval the block with params["foo_id"]' do
+        flexmock(@controller, :params => {'foo_id' => 'bar'})
         @controller.send :load_foo
-        assert_equal 'wibble', @controller.foo
-      end
-    end
-
-    context 'with a preloaded resource foo and a block' do
-      setup do
-        @controller.class.load_resource(:foo) do |game_id|
-          "test response #{game_id}"
-        end
-      end
-
-      context 'load_foo' do
-        should 'eval the block with params["foo_id"]' do
-          flexmock(@controller, :params => {'foo_id' => 'bar'})
-          @controller.send :load_foo
-          assert_equal 'test response bar', @controller.foo
-        end
+        assert_equal 'test response bar', @controller.instance_variable_get(:@foo)
       end
     end
   end
